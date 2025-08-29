62654fe424d4a54705f4ab1a332f033829734b04
Diego Pignattini <diego.pignattini@icloud.com>
2025-08-30 01:31:57 +0200

    First complete implementation of Collections class

--- linkwarden/collections.py
+++ linkwarden/collections.py
@@ -10,6 +10,7 @@ class Collections(Base):
         super().__init__(api_key, base_url, api_version)
         self.collections_endpoint = "/collections"
 
+
     def get_collections(self) -> Dict[str, Any]:
         """
         Get all collections
@@ -22,12 +23,13 @@ class Collections(Base):
         """
         return self._make_request("GET", self.collections_endpoint)
     
-    def get_collection(self, collection_id: str) -> Dict[str, Any]:
+
+    def get_collection(self, id: int) -> Dict[str, Any]:
         """
         Get a collection by ID
 
         Args:
-            collection_id: The ID of the collection to get
+            id: The ID of the collection to get
 
         Returns:
             Collection dictionary
@@ -35,14 +37,22 @@ class Collections(Base):
         Raises:
             APIError: If the API request fails
         """
-        return self._make_request("GET", f"{self.collections_endpoint}/{collection_id}")
+        return self._make_request("GET", f"{self.collections_endpoint}/{id}")
     
-    def create_collection(self, collection: Dict[str, Any]) -> Dict[str, Any]:
+
+    def create_collection(self, name: str, description: str = "", 
+                            color: str = "", icon: str = "", iconWeight: str = "", 
+                            parentId: int = None) -> Dict[str, Any]:
         """
         Create a collection
 
         Args:
-            collection: Collection dictionary
+            name: The name of the collection (required)
+            description: The description of the collection (optional)
+            color: The color of the collection (optional)
+            icon: The icon of the collection (optional)
+            iconWeight: The weight of the icon (optional)
+            parentId: The ID of the parent collection (optional)
 
         Returns:
             Collection dictionary
@@ -50,5 +60,55 @@ class Collections(Base):
         Raises:
             APIError: If the API request fails
         """
-        #TODO: Anche qui usare variabili del body predefinite invece che un generico dict? con dataclass?
-        #name, description, color, icon, iconWeight, parentId
+        payload = {
+            "name": name,
+            "description": description,
+            "color": color,
+            "icon": icon,
+            "iconWeight": iconWeight,
+            "parentId": parentId
+        }
+
+        return self._make_request("POST", self.collections_endpoint, json=payload)
+    
+
+    def update_collection(self, id: int, **kwargs) -> Dict[str, Any]:
+        """
+        Update a collection by ID
+
+        Args:
+            id: The ID of the collection to update
+            **kwargs: Fields to update, only the provided fields will be updated
+            Updateable fields: 
+                - name 
+                - description
+                - color
+                - icon
+                - iconWeight
+                - parentId 
+                - isPublic
+
+        Returns:
+            Collection dictionary
+
+        Raises:
+            APIError: If the API request fails
+        """
+        return self._make_request("PUT", f"{self.collections_endpoint}/{id}", json=kwargs)
+    
+
+    def delete_collection(self, id: int) -> Dict[str, Any]:
+        """
+        Delete a collection by ID
+
+        Args:
+            id: The ID of the collection to delete
+
+        Returns:
+            API response
+
+        Raises:
+            APIError: If the API request fails
+        """
+        return self._make_request("DELETE", f"{self.collections_endpoint}/{id}")
+    
